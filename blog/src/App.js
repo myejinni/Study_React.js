248665fcb6b21ddcf86e0578bbf56164c798208f
myejinni <bijou__ye@swu.ac.kr>
2023-04-03 19:45:46 +0900

    [React] component 문법

--- blog/src/App.js
+++ blog/src/App.js
@@ -51,9 +51,22 @@ function App() {
           <p>상세내용</p>
         </div>
 
+    <Modal></Modal>
+    <Alert></Alert>
 
     </div>
   );
+}
+
+function Modal(){
+
+  return(
+    <div className='modal'>
+          <h4>제목</h4>
+          <p>날짜</p>
+          <p>상세내용</p>
+    </div>
+  )
 }
 
 export default App;
